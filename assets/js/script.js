e5440b9702fe46ca6d2d2702e8413fb7d2ab9c77
seanorourke7 <>
2023-06-21 15:12:30 +0000

    create function to recognise gametype selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-let rock = document.getElementById("rock");
-rock.addEventListener("click", function () {
-    alert("working");
-});
+let gameType = document.getElementsByClassName("icons");
 
+for (let userSelection of gameType) {
+    userSelection.addEventListener("click", function () {
+        if (this.getAttribute("ID") === "rock") {
+            alert("rock");
+        } else {
+            alert("not rock");
+        }
+    });
+}
 
