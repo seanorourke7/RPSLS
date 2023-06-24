90a594ab1f9065d8c1f0bc1cbd3b85d44cc2a4f5
seanorourke7 <>
2023-06-24 09:51:56 +0000

    add function for cpu display box

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,19 @@ for (let gameTile of gameType) {
     gameTile.addEventListener("click", function () {
         let selectedIcon = this.getAttribute("ID");
         displayBox(selectedIcon);
+        cpuChoice();
+
     });
 
 };
 
 function displayBox(selectedIcon) {
     document.getElementById("result-display").innerHTML = `<button class="icons" id=${selectedIcon} type="submit"></button>`;
+}
+
+function cpuChoice() {
+    let tileArray = ["rock", "paper", "scissors", "lizard", "spock"];
+    let ranNumber = Math.floor(Math.random() * 4);
+    let cpuChoice = tileArray[ranNumber];
+    document.getElementById("cpu-display").innerHTML = `<button class="icons" id=${cpuChoice} type="submit"></button>`;
 }
