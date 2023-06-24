58252bb9a353392ed79d600ae27aaa9bc55c97a6
seanorourke7 <>
2023-06-24 11:39:13 +0000

    add checkanswer function to compare user with cpu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 let gameType = document.getElementsByClassName("icons");
-
 for (let gameTile of gameType) {
     gameTile.addEventListener("click", function () {
         let selectedIcon = this.getAttribute("ID");
         displayBox(selectedIcon);
-        cpuChoice();
-
+        cpuTile();
+        checkAnswer();
     });
 
 };
@@ -14,9 +13,18 @@ function displayBox(selectedIcon) {
     document.getElementById("result-display").innerHTML = `<button class="icons" id=${selectedIcon} type="submit"></button>`;
 }
 
-function cpuChoice() {
+function cpuTile() {
     let tileArray = ["rock", "paper", "scissors", "lizard", "spock"];
     let ranNumber = Math.floor(Math.random() * 4);
     let cpuChoice = tileArray[ranNumber];
     document.getElementById("cpu-display").innerHTML = `<button class="icons" id=${cpuChoice} type="submit"></button>`;
+
+}
+
+function checkAnswer() {
+    let userChoice = document.getElementById("result-display").innerHTML;
+    let aiChoice = document.getElementById("cpu-display").innerHTML;
+    if (userChoice === aiChoice) {
+        alert("DRAW");
+    }
 }
